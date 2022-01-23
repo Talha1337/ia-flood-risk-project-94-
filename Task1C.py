2143b759038fb4648bea65b9e5ccd18d8aab6d55
Talha Javed <taj29@cam.ac.uk>
2022-01-23 10:51:48 +0000

    Task 1C done, geo updated

---
+++ Task1C.py
@@ -0,0 +1,5 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_within_radius
+stations = build_station_list()
+print(stations_within_radius(stations, (52.2053, 0.1218), 10))
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -13,3 +13,16 @@ def stations_by_distance(stations,p):
         stationandlist = (station.name, haversine(station.coord, p))
         distancelist.append(stationandlist)
     return sorted_by_key(distancelist, 1)
+def stations_within_radius(stations, centre, r):
+    namelist = []
+    distancelist = stations_by_distance(stations, centre)
+    disttemplate = distancelist.copy()
+    for x in disttemplate:
+        if x[1] < r:
+            pass
+        else:
+            distancelist.remove(x)
+    sortedlist = sorted_by_key(distancelist, 0)
+    for y in sortedlist:
+        namelist.append(y[0])
+    return namelist

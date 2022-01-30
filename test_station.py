8996b965257b286afd90f2c43294058229590456
Talha Javed <taj29@cam.ac.uk>
2022-01-30 22:14:43 +0000

    changed inconsistent_typical_range_stations test (it failed first time)

--- test_station.py
+++ test_station.py
@@ -30,5 +30,5 @@ def test_create_monitoring_station():
 
 def test_inconsistent_typical_range_stations():
     stations = build_station_list()
-    assert len(inconsistent_typical_range_stations(stations)) > 0
     assert sorted(inconsistent_typical_range_stations(stations)) == inconsistent_typical_range_stations(stations)
+    assert inconsistent_typical_range_stations(stations) != None

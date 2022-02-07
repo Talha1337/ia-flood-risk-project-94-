7d4bbfc457f17abe9efd794c9e1fa10d580e2cd1
ariefhafizi <abh48@cam.ac.uk>
2022-02-07 12:38:14 +0000

    Task 1E comments added

--- Task1E.py
+++ Task1E.py
@@ -1,9 +1,11 @@
+"""importing functions that might be used"""
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_by_station_number
-stations = build_station_list()
+stations = build_station_list() #list of stations generated
 rivernames = rivers_with_station(stations)
 
 print("*** Task 1E: CUED Part IA Flood Warning System ***")
-print(rivers_by_station_number(stations, 9))
+"""Print out the 9 rivers with the most stations"""
+print(rivers_by_station_number(stations, 9)) 

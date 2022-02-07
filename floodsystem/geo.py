e9a2aab369e1682f1b8f786fb02ae86b1515918c
ariefhafizi <abh48@cam.ac.uk>
2022-02-07 13:05:24 +0000

    Task 1E comments improved

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -63,10 +63,7 @@ def rivers_by_station_number(stations, N):
     """creating tuples of rivers and number of stations"""
     river_stationnumber_list = []
     for river in river_list:
-<<<<<<< HEAD
-        len(river)
-=======
->>>>>>> 4488e0a (Task 1E fix)
+        """Create a list of tuples containing the rivername and number of stations"""
         x = (river, len(river_list[river]))
         river_stationnumber_list.append(x)
     """sorting by number of stations"""

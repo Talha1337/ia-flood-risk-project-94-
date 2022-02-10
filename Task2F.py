beea69d5187c6b8f3dfa2155865509e2ec005cc6
Talha1337 <98235118+Talha1337@users.noreply.github.com>
2022-02-10 17:41:56 +0000

    Small changes to task 2F and 2E, removed the 666 output

--- Task2F.py
+++ Task2F.py
@@ -6,9 +6,5 @@ from floodsystem.stationdata import update_water_levels
 import datetime
 stations = build_station_list()
 update_water_levels(stations)
-y = 0
 for x in stations_highest_rel_level_noname(stations, 5):
-    if y < 1:
-        y += 1
-    else:
-        plot_water_level_with_fit(x[0], datetime.timedelta(2), x[0].typical_range, 4)
+    plot_water_level_with_fit(x[0], datetime.timedelta(2), x[0].typical_range, 4)
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -3,7 +3,7 @@ def stations_level_over_threshold(stations, tol):
     list_of_over_threshold = []
     for station in stations:
         name_and_relative_water = []
-        if station.relative_water_level() == None:
+        if station.relative_water_level() == None or station.relative_water_level() > 50:
             pass
         elif station.relative_water_level() > tol:
             name_and_relative_water.append(station.name)
@@ -19,7 +19,7 @@ def stations_level_over_threshold_noname(stations, tol):
     list_of_over_threshold = []
     for station in stations:
         name_and_relative_water = []
-        if station.relative_water_level() == None:
+        if station.relative_water_level() == None or station.relative_water_level() > 50:
             pass
         elif station.relative_water_level() > tol:
             name_and_relative_water.append(station)
